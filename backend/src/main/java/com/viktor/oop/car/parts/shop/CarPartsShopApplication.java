5901c1aa08cee2681d2dff5d7c5c85931c6888bb
reipo38 <v03a01raik@pm.me>
2025-04-16 14:16:46 +0300

    init commit

---
+++ backend/src/main/java/com/viktor/oop/car/parts/shop/CarPartsShopApplication.java
@@ -0,0 +1,13 @@
+package com.viktor.oop.car.parts.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarPartsShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarPartsShopApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/viktor/oop/car/parts/shop/CarPartsShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viktor.oop.car.parts.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarPartsShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
